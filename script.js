63273b0bcbae1f049cd8ccbef5c65cbf22594c84
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-22 15:56:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('button').addEventListener('click', buttonOne)
+
+let age = 0
+let day = 'Monday'
+
+function buttonOne () {
+  day = document.getElementById('day').value
+
+  age = document.getElementById('age').value
+  age = parseInt(age)
+
+  if ((day != 'Saturday' && day != 'Sunday') && (age < 18)) {
+    document.getElementById('text').innerHTML = 'Time for school!'
+  } else if ((day != 'Saturday' && day != ' Sunday') && (age >= 18)) {
+    document.getElementById('text').innerHTML = 'Time to go to work!'
+  } else {
+    document.getElementById('text').innerHTML = 'Time to relax for the weekend!'
+  }
+}
